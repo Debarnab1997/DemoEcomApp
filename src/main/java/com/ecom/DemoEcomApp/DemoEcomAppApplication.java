4ae5cc53e047a12ffb6c74c7d52bbb7fcb2360eb
Debarnab Dasgupta <debarnab.dasgupta@nagarro.com>
2024-09-14 23:58:27 -0600

    Added initial files

---
+++ src/main/java/com/ecom/DemoEcomApp/DemoEcomAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.DemoEcomApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEcomAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEcomAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecom/DemoEcomApp/DemoEcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.DemoEcomApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
